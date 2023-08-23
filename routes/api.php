e15e117ffdc76dcfdc35efacce14251fa8381aba
almoshammer <dkoqhk13013@gmail.com>
2023-08-24 01:29:55 +0300

    Initical commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Xml\Tests;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+Route::get("/test", function ($request) {
+
+    $data = Tests::all();
+    return response()->json($data);
+});
+
+
+Route::post('/test/insert', function ($request) {
+    $data = $request->validate([
+        'test_name' => 'string',
+        'test_date' => 'date'
+    ]);
+
+    $result = Tests::create($data);
+    return response("Data Inserted Successfull");
 });
